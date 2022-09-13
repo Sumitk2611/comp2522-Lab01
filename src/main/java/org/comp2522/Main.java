dffc9fbb5f45459efcc9b73b62e45eb669a28483
Sumit <veerkhanduri@gmail.com>
2022-09-13 09:14:43 -0700

    fixed checkstyles

--- src/main/java/org/comp2522/Main.java
+++ src/main/java/org/comp2522/Main.java
@@ -1,7 +1,19 @@
 package org.comp2522;
 
+/**
+ * Main Java.
+ *
+ * @author Sumit.
+ *
+ * @version 2022.
+ */
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello Panda!");
-    }
+  /**
+  * Runs the program.
+  *
+  * @param args null
+  */
+  public static void main(String[] args) {
+    System.out.println("Hello Panda!");
+  }
 }
